146ffed684f3ca52df2c83a33095ff7720d9ecaa
code-with-adhi <125806079+code-with-adhi@users.noreply.github.com>
2025-08-16 23:57:35 +0530

    Trying new things

---
+++ others/SlidingWindow.java
@@ -0,0 +1,10 @@
+class SlidingWindow{
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[] slider(int[] arr){
+        int[] res = {};
+        return res;
+    }
+}
